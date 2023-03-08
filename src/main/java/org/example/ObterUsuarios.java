9a202bcc066d315d7b61f7046143c61beb4d29ca
MarceloSLeal <marcelosleal@outlook.com>
2023-03-08 02:01:22 -0300

    Pesquisar vários usuários pelo jpa e transferindo para uma lista de usuários

---
+++ src/main/java/org/example/ObterUsuarios.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
+public class ObterUsuarios {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Exercicios-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        String jpql = "SELECT u FROM Usuario u";
+        TypedQuery<Usuario> query = em.createQuery(jpql, Usuario.class);
+        query.setMaxResults(5);
+
+        List<Usuario> usuarios = query.getResultList();
+
+        for (Usuario usuario : usuarios) {
+            System.out.println(usuario.getId() + " " + usuario.getNome() + " " + usuario.getEmail());
+        }
+
+        em.close();
+        emf.close();
+    }
+}
