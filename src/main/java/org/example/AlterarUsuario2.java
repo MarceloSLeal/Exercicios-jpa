906f1ef48fd2ad20d9de264fa6977a5a73ba2c5d
MarceloSLeal <marcelosleal@outlook.com>
2023-03-09 02:48:14 -0300

    Alterando o objeto em modo transacional mesmo não executando o merge ainda assim é feita a atualização no banco

---
+++ src/main/java/org/example/AlterarUsuario2.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class AlterarUsuario2 {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Exercicios-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Usuario usuario = em.find(Usuario.class, 7L);
+        usuario.setNome("MarceloLeal");
+        usuario.setEmail("marceloleal@email.com");
+
+//        em.merge(usuario);
+
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+    }
+
+}
