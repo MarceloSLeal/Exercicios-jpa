69f6ee16a48b0abe48539120dba201341201daf9
MarceloSLeal <marcelosleal@outlook.com>
2023-03-09 02:57:15 -0300

    Usando detach para desativar o moto transacional e reativando com o merge para fazer a alteração na entidade

---
+++ src/main/java/org/example/AlterarUsuario3.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class AlterarUsuario3 {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("Exercicios-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Usuario usuario = em.find(Usuario.class, 7L);
+        em.detach(usuario);
+
+        usuario.setNome("MarceloLealIdiota");
+        em.merge(usuario);
+
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+
+    }
+}
